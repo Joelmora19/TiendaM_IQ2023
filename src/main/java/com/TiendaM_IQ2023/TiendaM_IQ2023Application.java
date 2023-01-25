3e039dc4c5cc436fb752acd5c3ff2d8d09fbb127
Joelmora19 <108998021+Joelmora19@users.noreply.github.com>
2023-01-25 15:51:54 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/TiendaM_IQ2023/TiendaM_IQ2023Application.java
@@ -0,0 +1,15 @@
+package com.TiendaM_IQ2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TiendaM_IQ2023Application {
+    
+        public static void main(String[]args){
+            ConfigurableApplicationContext run = SpringApplication.run(TiendaM_IQ2023Application.class, args);
+        }
+    
+    
+}
